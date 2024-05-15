52e39d0d9f5af493798805fab95a344e668e34f7
Goofygiraffe06 <rahul.narsing06@gmail.com>
2024-05-15 16:14:37 +0530

    Added base bot template for cogs

---
+++ Monster-Worm/main.py
@@ -0,0 +1,42 @@
+import discord
+import logging
+from discord.ext import commands
+import os
+import asyncio
+
+# Initialize logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+# Suppress Flask development server log
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
+logging.getLogger("flask.app").setLevel(logging.ERROR)
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+async def main():
+    async with bot:
+        # Load cogs
+        for filename in os.listdir("./cogs"):
+            # utils.py is a non cog file and used to import neccesary functions
+            if (
+                filename.endswith(".py")
+                and filename != "__init__.py"
+                and filename != "utils.py"
+                and filename != "db_utils.py"
+            ):
+                try:
+                    await bot.load_extension(f"cogs.{filename[:-3]}")
+                    logging.info(f"Loaded {filename} cog successfully.")
+                except Exception as e:
+                    logging.error(f"Error loading {filename}: {e}")
+        await bot.start(os.environ["token"])
+
+
+try:
+    asyncio.run(main())
+except (discord.errors.HTTPException, discord.app_commands.errors.CommandInvokeError):
+    logging.error("Being Rate Limited!")
+    os.system("kill 1")  # Kill the current running process
+    os.system("python restart.py")
