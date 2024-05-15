5dde74f3c8a9b290717c43be72a25f72245d35d7
Goofygiraffe06 <rahul.narsing06@gmail.com>
2024-05-15 16:56:12 +0530

    Fixed indentation error

--- Monster-Worm/cogs/on_ready.py
+++ Monster-Worm/cogs/on_ready.py
@@ -6,11 +6,11 @@ class onReady(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.Cog.listener()
-    async def on_ready(self):
-        logging.info(f"We have logged in as {self.bot.user}")
-        try:
-            synced = await self.bot.tree.sync()
-            logging.info(f"Synced {len(synced)} command(s)...")
-        except Exception as e:
-            logging.error(f"Error syncing commands!: {e}")
+@commands.Cog.listener()
+async def on_ready(self):
+    logging.info(f"We have logged in as {self.bot.user}")
+    try:
+        synced = await self.bot.tree.sync()
+        logging.info(f"Synced {len(synced)} command(s)...")
+    except Exception as e:
+        logging.error(f"Error syncing commands!: {e}")
