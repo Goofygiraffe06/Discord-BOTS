89a8af7e54789f3fbbabdf471bcd59e3404a86e6
Goofygiraffe06 <rahul.narsing06@gmail.com>
2024-05-15 16:58:52 +0530

    Added mising setup function

--- Monster-Worm/cogs/on_ready.py
+++ Monster-Worm/cogs/on_ready.py
@@ -6,11 +6,14 @@ class onReady(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-@commands.Cog.listener()
-async def on_ready(self):
-    logging.info(f"We have logged in as {self.bot.user}")
-    try:
-        synced = await self.bot.tree.sync()
-        logging.info(f"Synced {len(synced)} command(s)...")
-    except Exception as e:
-        logging.error(f"Error syncing commands!: {e}")
+	@commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f"We have logged in as {self.bot.user}")
+        try:
+            synced = await self.bot.tree.sync()
+            logging.info(f"Synced {len(synced)} command(s)...")
+        except Exception as e:
+            logging.error(f"Error syncing commands!: {e}")
+
+async def setup(bot) -> None:
+    await bot.add_cog(onReady(bot))
